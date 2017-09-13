5ebe11650f77cd39a4a425cc07786e187834f0e2
malmax <malmax.spb@gmail.com>
2017-09-13 17:33:43 +0300

    init decoding

--- solution.js
+++ solution.js
@@ -1,47 +1,107 @@
-'use strict';
-
-// if (typeof window === 'undefined') var window = global.window;
-
 function drawNestedSetsTree(data, mountPoint) {
   let virtualTree = {};
 
   function parseNestedSet() {
-    const { minLeft, maxRight } = data.reduce(
-      ({ minLeft, maxRight }, item) => {
-        if (item.left < minLeft) minLeft = item.left;
-        if (item.right > maxRight) maxRight = item.right;
-        return { minLeft, maxRight };
+    const { left: minLeft, right: maxRight } = data.reduce(
+      ({ left: lastLeft, right: lastRight }, item) => {
+        let left = lastLeft;
+        let right = lastRight;
+        if (item.left < left) left = item.left;
+        if (item.right > right) right = item.right;
+        return {
+          left,
+          right,
+        };
       },
-      { minLeft: Infinity, maxRight: 0 },
+      {
+        left: Infinity,
+        right: 0,
+      },
     );
-
-    console.log(minLeft, maxRight);
 
     if (maxRight <= minLeft) throw new Error('Nested Set error');
 
     function getChildren(item, parent) {
+      if (!item.title) {
+        console.log(item);
+        return false;
+      }
+
+      const id = `${item.left}_${item.right}`;
+      const domElement = document.createElement('li');
+      domElement.setAttribute('id', id);
+      domElement.innerText = item.title;
+
+      const childrenContainer = document.createElement('ul');
+
+      if (parent) {
+        parent.childrenContainer.appendChild(domElement);
+      }
+
       const out = {
         title: item.title,
-        id: `${item.left}_${item.right}`,
-        children: [],
+        domElement,
+        childrenContainer,
+        id,
+        parent,
+        childrens: [],
       };
-      for (let i = item.left + 1; i < item.right; i++) {
-        const children = data.filter(iterator => iterator.left === i)[0];
-        out.children.push(getChildren(children));
-        i = children.right;
+
+      if (item.right - item.left > 1) {
+        if (parent) {
+          parent.childrenContainer.appendChild(childrenContainer);
+        }
+
+        for (let i = item.left + 1; i < item.right; i += 1) {
+          const children = data.filter(elem => elem.left === i)[0];
+          out.childrens.push(getChildren(children, out));
+          i = children.right;
+        }
       }
 
       return out;
     }
 
-    const root = data.filter(iterator => iterator.left === minLeft)[0];
+    const root = data.filter(elem => elem.left === minLeft)[0];
 
-    virtualTree = getChildren(root);
+    virtualTree = getChildren(root, { childrenContainer: mountPoint });
 
     console.log(virtualTree);
   }
 
+  function callElemTree(element, callback) {
+    console.log(element);
+    callback(element);
+    element.childrens.forEach(elem => callElemTree(elem, callback));
+  }
+
   parseNestedSet(data);
+
+  let start = 0;
+  const plainTree = [];
+  callElemTree(virtualTree, (itemVirtualTree) => {
+    start += 1;
+    plainTree.push({
+      title: itemVirtualTree.title,
+      left: start,
+      right: start + 1,
+    });
+
+    if (itemVirtualTree.parent.title) {
+      let parent = itemVirtualTree.parent;
+      do {
+        plainTree.forEach((iterator) => {
+          if (iterator.title === parent.title) {
+            iterator.right = iterator.right > start + 1 ? iterator.right : start + 1;
+          }
+        });
+        if (parent.parent.title) parent = parent.parent;
+        else parent = false;
+      } while (parent);
+    }
+  });
+
+  console.log(plainTree);
 }
 
 const data = [
@@ -102,4 +162,4 @@ const data = [
   },
 ];
 
-const tree = drawNestedSetsTree(data, window.document.getElementById('root'));
+const tree = drawNestedSetsTree(data, document.getElementById('root'));
